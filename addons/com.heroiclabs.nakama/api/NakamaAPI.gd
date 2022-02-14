9f07e87ef45ff0729083f43e17971b0bfa99b2e6
Jason Knight <jason@winterpixel.com>
2022-02-14 07:12:34 -0600

    Fix http key

--- addons/com.heroiclabs.nakama/api/NakamaAPI.gd
+++ addons/com.heroiclabs.nakama/api/NakamaAPI.gd
@@ -5170,7 +5170,7 @@ class ApiClient extends Reference:
 		if p_payload != null:
 			query_params += "payload=%s&" % NakamaSerializer.escape_http(p_payload)
 		if p_http_key != null:
-			query_params += "httpKey=%s&" % NakamaSerializer.escape_http(p_http_key)
+			query_params += "http_key=%s&" % NakamaSerializer.escape_http(p_http_key)
 		var uri = "%s%s%s" % [_base_uri, urlpath, "?" + query_params if query_params else ""]
 		var method = "GET"
 		var headers = {}
@@ -5197,7 +5197,7 @@ class ApiClient extends Reference:
 		urlpath = urlpath.replace("{id}", NakamaSerializer.escape_http(p_id))
 		var query_params = ""
 		if p_http_key != null:
-			query_params += "httpKey=%s&" % NakamaSerializer.escape_http(p_http_key)
+			query_params += "http_key=%s&" % NakamaSerializer.escape_http(p_http_key)
 		var uri = "%s%s%s" % [_base_uri, urlpath, "?" + query_params if query_params else ""]
 		var method = "POST"
 		var headers = {}
