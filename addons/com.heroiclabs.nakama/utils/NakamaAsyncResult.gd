60d63573eb64aa62183d45d8769243b56189646e
Jayden Sipe <jayden.sipe@gmail.com>
2023-03-29 11:33:23 -0400

    Fix type check and typehint for Godot 4.0-rc3 (#157)
    * Use `is_instance_of()` rather than `is` for type check
    * Updated `_put_packet_script()` to typehint return `Error` instead of `int`

--- addons/com.heroiclabs.nakama/utils/NakamaAsyncResult.gd
+++ addons/com.heroiclabs.nakama/utils/NakamaAsyncResult.gd
@@ -27,7 +27,7 @@ func _to_string():
 	return "NakamaAsyncResult<>"
 
 static func _safe_ret(p_obj, p_type : GDScript):
-	if p_obj is p_type:
+	if is_instance_of(p_obj, p_type):
 		return p_obj # Correct type
 	elif p_obj is NakamaException:
 		return p_type.new(p_obj) # It's an exception. Incapsulate it
